5629304f32228361429c4478377ddd9284959f7d
Abhishek <136082512+pandey-jee@users.noreply.github.com>
2024-09-18 19:34:04 +0530

    Update Mint_burn.sol

--- Mint_burn.sol
+++ Mint_burn.sol
@@ -1,57 +1,31 @@
-// SPDX-License-Identifier: GPL - 3.0
-pragma solidity ^0.8.18;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract MyToken {
-    address public owner;
-    string public tokenName;
-    string public tokenSymbol;
-    uint8 public tokenDecimals;
-    uint256 public tokenTotalSupply;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-    // Mapping to store balances of addresses
-    mapping(address => uint256) public balance;
+contract MyToken is ERC20 {
+    address public admin;
 
-    // Events to log the transactions
-    event Mint(address indexed to, uint256 amount);
-    event Transfer(address indexed from, address indexed to, uint256 amount);
-    event Burn(address indexed from, uint256 amount);
-
-    constructor() {
-        owner = msg.sender;
-        tokenName = "Cipher";
-        tokenSymbol = "Cyp";
-        tokenDecimals = 10;
-        tokenTotalSupply = 0;
+    constructor(uint256 supply) ERC20("MyToken", "MYT") {
+        _mint(msg.sender, supply);
+        admin = msg.sender;
     }
 
-    // Modifier to restrict minting to the contract owner only
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can mint tokens.");
+    function generateTokens(address recipient, uint256 value) public onlyAdmin {
+        _mint(recipient, value);
+    }
+
+    function destroyTokens(uint256 value) public {
+        _burn(msg.sender, value);
+    }
+
+    function moveTokens(address sender, address recipient, uint256 value) public returns (bool) {
+        _transfer(sender, recipient, value);
+        return true;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can generate tokens");
         _;
-    }
-
-    // Function to mint new tokens
-    function mintTokens(address to, uint256 amount) public onlyOwner {
-        tokenTotalSupply += amount;
-        balance[to] += amount;
-        emit Mint(to, amount);
-    }
-
-    // Function to transfer tokens
-    function transferTokens(address receiver, uint256 amount) public {
-        require(balance[msg.sender] >= amount, "Insufficient balance to transfer tokens.");
-
-        balance[msg.sender] -= amount;
-        balance[receiver] += amount;
-        emit Transfer(msg.sender, receiver, amount);
-    }
-
-    // Function to burn tokens
-    function burnTokens(uint256 amount) public {
-        require(balance[msg.sender] >= amount, "Insufficient balance to burn tokens.");
-
-        tokenTotalSupply -= amount;
-        balance[msg.sender] -= amount;
-        emit Burn(msg.sender, amount);
     }
 }
